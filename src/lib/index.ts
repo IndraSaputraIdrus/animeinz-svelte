a810fd14c40176323ef676baef1869bff8501091
indra saputra idrus <indrasaputraidrus@gmail.com>
2024-07-16 00:51:43 +0800

    init

---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+// place files you want to import through the `$lib` alias in this folder.
+export const baseUrl = 'https://otakudesu.cloud'
+export const nonce = "758b1de264"
+export const actionId = "2a3505c93b0035d3f455df82bf976b84"
