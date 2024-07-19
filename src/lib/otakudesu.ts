e0641f1454bd83d679ec608571e545fb497d7fa6
indra saputra idrus <indrasaputraidrus@gmail.com>
2024-07-19 17:00:39 +0800

    Fix: dynamic nonce key

--- src/lib/otakudesu.ts
+++ src/lib/otakudesu.ts
@@ -1,11 +1,11 @@
-import { baseUrl, nonce } from "$lib"
-import { fail, redirect } from "@sveltejs/kit";
+import { baseUrl } from "$lib"
 import { parseHTML } from "linkedom";
 
 type GetStreamLink = {
   id: number;
   i: number;
-  q: string
+  q: string,
+  nonce: string;
 }
 
 export const getNonce = async () => {
@@ -13,15 +13,20 @@ export const getNonce = async () => {
     method: "POST",
     body: new URLSearchParams({ action: "aa1208d27f29ca340c92c66d1926f13f" }).toString(),
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
+      "Content-Type": "application/x-www-form-urlencoded",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
     }
   })
 
-  const data = await res.text()
-  console.log(data)
+  if (!res.ok) {
+    return null
+  }
+
+  const { data } = await res.json()
+  return data as string
 }
 
-export const getStreamUrl = async ({ i, id, q }: GetStreamLink) => {
+export const getStreamUrl = async ({ i, id, q, nonce }: GetStreamLink) => {
   const res = await fetch(`${baseUrl}/wp-admin/admin-ajax.php`, {
     method: "POST",
     body: new URLSearchParams({
